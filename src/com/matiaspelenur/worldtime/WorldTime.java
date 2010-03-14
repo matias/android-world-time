9b059ceb86c461541042e4c9d0fea667f74a4b10
Matias Pelenur <matias@Harvey.local>
2010-03-13 23:25:36 -0500

    much better

--- src/com/matiaspelenur/worldtime/WorldTime.java
+++ src/com/matiaspelenur/worldtime/WorldTime.java
@@ -4,7 +4,7 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-import java.util.Map;
+import java.util.Set;
 import java.util.TimeZone;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -30,10 +30,10 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.ViewSwitcher;
 
+import com.google.common.collect.ArrayListMultimap;
 import com.google.common.collect.ImmutableList;
-import com.google.common.collect.ImmutableMap;
-import com.google.common.collect.LinkedHashMultimap;
-import com.google.common.collect.Lists;
+import com.google.common.collect.ListMultimap;
+import com.google.common.collect.Sets;
 
 public class WorldTime extends Activity {
 
@@ -41,11 +41,8 @@ public class WorldTime extends Activity {
   private ListView mainListView;
   private ExpandableListView timeZonesListView;
   private ViewSwitcher switcher;
-  private LinkedHashMultimap<String,String> timeZonesByRegion;
   
-  // TODO(matias): let the user enable or disable regions
-  private static final List<String> enabledRegions = ImmutableList.of(
-      "America", "Europe"/*, "Africa", "Indian", "Pacific"*/);
+  private Set<String> enabledCities = Sets.newHashSet();
   
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -71,64 +68,44 @@ public class WorldTime extends Activity {
   @Override
   public boolean onOptionsItemSelected(MenuItem item) {
     if (timeZonesListView.getAdapter() == null) {
-//      fillTZList();
       timeZonesListView.setAdapter(new RegionCityListAdapter());
     }
     switcher.showNext();
     return true;
   }
 
-  private void fillTZList() {
-    if (timeZonesByRegion == null) {
-      populateTZRegionMap();
-    }
-    
-    List<Map<String, String>> regionList = Lists.newArrayList();
-    List<List<Map<String, String>>> cityList = Lists.newArrayList();
-    int id = 0;
-    for (String region: enabledRegions) {
-      regionList.add(ImmutableMap.of("region", region, "_id", String.valueOf(id)));
-      id++;
-      List<Map<String, String>> cities = Lists.newArrayList();
-      for (String city : timeZonesByRegion.get(region)) {
-        cities.add(ImmutableMap.of("city", city, "_id", String.valueOf(id)));
-        id++;
-      }
-      cityList.add(cities);
-    }
-//    Log.i(this.toString(), String.valueOf(regionList));
-//    Log.i(this.toString(), String.valueOf(cityList));
-    
-    /*SimpleExpandableListAdapter listAdapter = new SimpleExpandableListAdapter(
-        this,
-        regionList,
-        R.layout.regions_parent_row,
-        new String[] { "_id", "region" },
-        new int[] { R.id.region_name},
-        cityList,
-        R.layout.regions_child_row,
-        new String[] { "_id", "city" },
-        new int[] { R.id.city_name }
-        );
-    timeZonesListView.setAdapter(listAdapter);*/
-  }
-  
   public class RegionCityListAdapter extends BaseExpandableListAdapter {
 
-    private String[] regions = { "Africa", "Europe" };
-    private String[][] children = {
-        {"1", "2", "3", "4", "5", "6", "7", "8", "9"},
-        {"10","11","12","13","14","15","16","17","18"}
-    };
-    
-    private boolean[][] childrenValue = new boolean[regions.length][children[0].length];
+    // TODO(matias): let the user enable or disable regions
+    private final List<String> enabledRegions = ImmutableList.of(
+        "America", "Europe"/*, "Africa", "Indian", "Pacific"*/);
+
+    private ListMultimap<String,String> regionCityMap;
     
     public RegionCityListAdapter() {
+      populateRegionCityMap();
+    }
+    
+    private void populateRegionCityMap() {
+      regionCityMap = ArrayListMultimap.create();
+      Pattern tzPattern = Pattern.compile("(\\w+)/(\\w+)");
+      for (String tz : TimeZone.getAvailableIDs()) {
+        Matcher match = tzPattern.matcher(tz);
+        if (match.matches()) {
+          String region = match.group(1);
+          if (enabledRegions.contains(region)) {
+            String city = match.group(2);
+            regionCityMap.put(region, city);
+          }
+        }
+      }
     }
     
     @Override
     public Object getChild(int groupPosition, int childPosition) {
-      return children[groupPosition][childPosition];
+      String region = enabledRegions.get(groupPosition);
+      String city = regionCityMap.get(region).get(childPosition);
+      return city;
     }
 
     @Override
@@ -139,17 +116,25 @@ public class WorldTime extends Activity {
     @Override
     public View getChildView(final int groupPosition, final int childPosition, boolean isLastChild,
         View convertView, ViewGroup parent) {
+      String region = enabledRegions.get(groupPosition);
+      String city = regionCityMap.get(region).get(childPosition);
+      final String fullName = region + "/" + city;
+      
       LinearLayout layout = new LinearLayout(WorldTime.this);
       TextView textView = new TextView(WorldTime.this);
       textView.setText(getChild(groupPosition, childPosition).toString());
       
       CheckBox checkbox = new CheckBox(WorldTime.this);
-      checkbox.setChecked(childrenValue[groupPosition][childPosition]);
+      checkbox.setChecked(enabledCities.contains(fullName));
       checkbox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
         @Override
         public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
           Log.i("matias:", "isChecked=" + isChecked + " for " + buttonView);
-          childrenValue[groupPosition][childPosition] = isChecked;
+          if (isChecked) {
+            enabledCities.add(fullName);
+          } else {
+            enabledCities.remove(fullName);
+          }
         }
       });
       layout.addView(checkbox);
@@ -159,17 +144,17 @@ public class WorldTime extends Activity {
 
     @Override
     public int getChildrenCount(int groupPosition) {
-      return children[groupPosition].length;
+      return regionCityMap.size();
     }
 
     @Override
     public Object getGroup(int groupPosition) {
-      return regions[groupPosition];
+      return enabledRegions.get(groupPosition);
     }
 
     @Override
     public int getGroupCount() {
-      return regions.length;
+      return enabledRegions.size();
     }
 
     @Override
@@ -188,29 +173,14 @@ public class WorldTime extends Activity {
 
     @Override
     public boolean hasStableIds() {
-      // TODO Auto-generated method stub
-      return false;
+      return true;
     }
 
     @Override
     public boolean isChildSelectable(int groupPosition, int childPosition) {
-      // TODO Auto-generated method stub
       return false;
     }
     
-  }
-
-  private void populateTZRegionMap() {
-    timeZonesByRegion = LinkedHashMultimap.create();
-    Pattern tzPattern = Pattern.compile("(\\w+)/(\\w+)");
-    for (String tz : TimeZone.getAvailableIDs()) {
-      Matcher match = tzPattern.matcher(tz);
-      if (match.matches()) {
-        String region = match.group(1);
-        String city = match.group(2);
-        timeZonesByRegion.put(region, city);
-      }
-    }
   }
 
   @Override
